a57bc61f7e868d452287edc086ef3f08307154c1
reactor <reagan@feargarden.xyz>
2020-06-17 06:03:57 -0400

    index update

--- p/index/content.php
+++ p/index/content.php
@@ -1,7 +1,6 @@
 <p class="center"><img src="https://img.feargarden.xyz/site/about/shannon.gif" alt=""></p>
-<p>Welcome to <b>feargarden.xyz</b>. I'm Reagan, and this is my site. I used to host content here, but now it's more of a hub of sorts while I use my server for other things.</p>
-<p>I tend to mess around with HTML/CSS and PHP a lot, but I don't really do anything that's particularly amazing. In addition to working on this site, I'm also fairly active on Neocites.</p>
-<p>I can either be contacted by <a href="mailto:reag@tuta.io">email</a>, through <a href="https://scenecore.net/reag" target="_blank">the fediverse</a> (via any activitypub instance), or you can leave a message on my <a href="https://reagan.123guestbook.com/" target="_blank">guestbook</a>.
+<p>Welcome to <b>feargarden.xyz</b>. I'm Reagan, and this is my site.</p>
+<p>I can either be contacted by <a href="mailto:reagan@feargarden.xyz">email</a>, through <a href="https://scenecore.net/reag" target="_blank">the fediverse</a> (via any activitypub instance), or you can leave a message on my <a href="https://reagan.123guestbook.com/" target="_blank">guestbook</a>.
 <h2>on this site</h2>
 <p>
 	&#10014; <a href="blog/">blog</a>
