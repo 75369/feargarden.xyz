f5089b42bf7b33922a5a24a219c63208cec93447
reactor <reagan@feargarden.xyz>
2020-07-11 07:26:17 -0400

    Minor page edits, small fixes

--- gensite.sh
+++ gensite.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
-printf "# blog\n\n![header](/images/sayo.png)\n\nMy personal blog." > pages/blog.md
+printf "# blog\n\nMy personal blog." > pages/blog.md
 for file in posts/*; do
     blogid=$(basename "${file%.*}")
     title=$(head -1 $file | cut -c 3-) 
     date=$(sed '3q;d' $file | cut -c 4-)
     printf "\n\n* [$title]($blogid.html) $date" >> pages/blog.md
 done
+printf "\n\n->![header](/images/sayo.png)<-" >> pages/blog.md
 for file in pages/*; do
     filename="${file%.*}"
     markdown -o temp.html -F MKD_AUTOLINK "$file"
-    cat res/template_header.html temp.html res/template_footer.html > $filename.html
+    cat res/template_header.html res/template_page.html temp.html res/template_footer.html > $filename.html
     tidy -q -m $filename.html
     rm temp.html
 done
@@ -19,7 +20,7 @@ mv pages/*.html ./
 for file in posts/*; do
     filename="${file%.*}"
     markdown -o temp.html -F MKD_AUTOLINK "$file"
-    cat res/template_header_blog.html temp.html res/template_footer.html > $filename.html
+    cat res/template_header.html res/template_blog.html temp.html res/template_footer.html > $filename.html
     tidy -q -m $filename.html
     rm temp.html
 done
--- pages/contact.md
+++ pages/contact.md
@@ -2,19 +2,23 @@
 
 **The fediverse:**
 
-* scenecore.net: [@reag](https://scenecore.net/reag)
+* Pleroma: [@reag@scenecore.net](https://scenecore.net/reag)
 
-* systematic.love: [@reactor](https://systematic.love/@reactor)
+* Misskey: [@reactor@systematic.love](https://systematic.love/@reactor)
 
 **Direct:**
 
 * Discord: 961#8190
 
-* Email: [reagan@feargarden.xyz](mailto:reagan@feargarden.xyz) 
+* Email: [reagan@feargarden.xyz](mailto:reagan@feargarden.xyz), [reag@tuta.io](mailto:reag@tuta.io)
 
 * XMPP: [reactor@systematic.love](xmpp:reactor@systematic.love)
 
 **Other:**
+
+* Gitea: [reactor@git.feargarden.xyz](https://git.feargarden.xyz/reactor)
+
+* Github: [75369](https://github.com/75369)
 
 * Neocities: [reagnyan](https://neocities.org/sites/reagnyan)
 
